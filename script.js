c766cccf46a0e06bf3b7d13511eb688754d8c2ee
NicoleBrady <nicolebrady.dev@gmail.com>
2021-07-08 16:26:18 -0400

    completed ham menU

--- script.js
+++ script.js
@@ -6,4 +6,18 @@ $(document).ready(function(){
         }, 1000);
     });
 
+// mobile navigation
+    $(".mobile-nav-button").click(function() {
+        $(".mobile-nav-slider").fadeIn(300);
+        $(".mobile-nav-slider").css("display", "flex");
+            $(".mobile-nav-slider").css('left', '0%');
+            // $(".mobile-nav-slider").fadeIn("slow");
+    })
+
+    $(".close-button").click(function() {
+        $(".mobile-nav-slider").fadeOut(200, function() {
+        $(".mobile-nav-slider").css("display", "none");
+            $(".mobile-nav-slider").css('left', '100%'); 
+        });
+    })
 })
